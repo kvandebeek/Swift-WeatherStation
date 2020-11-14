c34d6f13bc3ad08626c9a17f8d866fabb5a7622d
Kristof Vandebeek <kristof.vandebeek@gmail.com>
2020-11-14 12:01:42 +0100

    Github webhook test

--- Weerstation/AppDelegate.swift
+++ Weerstation/AppDelegate.swift
@@ -9,8 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.       
        return true
-        
-        
+
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
